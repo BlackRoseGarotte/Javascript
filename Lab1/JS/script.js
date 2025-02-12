3c0277408e6cb07df3a9b6f2c77cbf9faca590c0
BlackRoseGarotte <balevvadim123@gmail.com>
2025-02-12 21:52:19 +0200

    second commit

--- Lab1/JS/script.js
+++ Lab1/JS/script.js
@@ -1,4 +1,12 @@
-console.log("Hello, world!");
-console.log(2+2);
-const el = document.getElementById('app');
-el.innerHTML += 'Alex!';
+let score = prompt("Введите ваш балл:");
+if (score >= 90) {
+ console.log("Отлично!");
+} else if (score >= 70) {
+ console.log("Хорошо");
+} else {
+ console.log("Можно лучше!");
+}
+
+for (let i = 1; i <= 5; i++) {
+ console.log(`Итерация: ${i}`);
+}
