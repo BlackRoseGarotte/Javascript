e8fa873865995a8f5f98a5e9c836900b41bdc1ef
BlackRoseGarotte <balevvadim123@gmail.com>
2025-02-12 23:38:55 +0200

    qq

--- Lab1/JS/script.js
+++ Lab1/JS/script.js
@@ -1,3 +1,14 @@
+alert("Этот код выполнен из внешнего файла!");
+console.log("Сообщение в консоли");
+
+name = "Vadim Balev"
+birthYear = 2005;
+isStudent = true;
+
+console.log(name);
+console.log(birthYear);
+console.log(isStudent);
+
 let score = prompt("Введите ваш балл:");
 if (score >= 90) {
  console.log("Отлично!");
